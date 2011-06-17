df6f87343f118969b454a3c93c0ef4ccd22fdc78
Amir Rajan <ar@amirrajan.net>
2011-06-17 17:57:52 -0500

    initial commit of Oak

---
+++ Oak/Models/Mix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Dynamic;
+
+namespace Oak
+{
+    public class Mix : DynamicObject
+    {
+        public dynamic MixWith { get; set; }
+
+        public Mix(ExpandoObject mixWith)
+        {
+            MixWith = mixWith;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var dictionary = MixWith as IDictionary<string, object>;
+
+            if (dictionary.ContainsKey(binder.Name))
+            {
+                result = dictionary[binder.Name];
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            var dictionary = MixWith as IDictionary<string, object>;
+
+            if (dictionary.ContainsKey(binder.Name))
+            {
+                dictionary[binder.Name] = value;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Oak/Models/Query.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using Massive;
+
+namespace Oak
+{
+    public static class Query
+    {
+        public static void ExecuteNonQuery(this string query, ConnectionProfile connectionProfile)
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = new SqlConnection(connectionProfile.ConnectionString);
+            sqlCommand.Connection.Open();
+            sqlCommand.CommandText = String.Format(query);
+            sqlCommand.ExecuteNonQuery();
+            sqlCommand.Connection.Close();
+        }
+
+        public static object ExecuteScalar(this string query, ConnectionProfile connectionProfile)
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = new SqlConnection(connectionProfile.ConnectionString);
+            sqlCommand.Connection.Open();
+            sqlCommand.CommandText = String.Format(query);
+            var result = sqlCommand.ExecuteScalar();
+            sqlCommand.Connection.Close();
+
+            return result;
+        }
+
+        public static SqlDataReader ExecuteReader(this string query, ConnectionProfile connectionProfile)
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = new SqlConnection(connectionProfile.ConnectionString);
+            sqlCommand.Connection.Open();
+            sqlCommand.CommandText = String.Format(query);
+            return sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        public static void InsertInto(this object o, string table, ConnectionProfile connectionProfile)
+        {
+            DynamicModel dynamicModel = new DynamicModel(connectionProfile, table, "Id");
+
+            dynamicModel.Insert(o);
+        }
+    }
+}
