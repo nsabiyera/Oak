5b489593d61d41e1645e14fe9a7bc5a019ab7cf2
Amir Rajan <ar@amirrajan.net>
2011-07-09 16:30:41 -0500

    added sample project to show off oak

---
+++ DynamicBlog/DynamicBlog.Tests/Controllers/describe_HomeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSpec;
+using DynamicBlog.Controllers;
+using System.Web.Mvc;
+using Moq;
+using DynamicBlog.Models;
+using NUnit.Framework;
+
+namespace DynamicBlog.Tests.Controllers
+{
+    class describe_HomeController : nspec
+    {
+        HomeController homeController;
+        ActionResult actionResult;
+        dynamic viewBag;
+        Mock<Blogs> blogs;
+        List<dynamic> blogsFromModel;
+        string titleToSave;
+        string bodyToSave;
+        List<dynamic> insertedBlogs;
+
+        void before_each()
+        {
+            blogs = new Mock<Blogs>();
+            blogsFromModel = new List<dynamic> { new { } };
+            blogs.Setup(s => s.All()).Returns(blogsFromModel);
+            homeController = new HomeController(blogs.Object);
+            insertedBlogs = new List<dynamic>();
+            blogs.Setup(s => s.Insert(It.IsAny<object>())).Callback<object>(b => insertedBlogs.Add(b));
+        }
+
+        void navigating_to_the_home_page()
+        {
+            act = () =>
+            {
+                actionResult = homeController.Index();
+                viewBag = (actionResult as ViewResult).ViewBag;
+            };
+
+            it["gives a list of blogs"] = () =>
+                (viewBag.Blogs as IEnumerable<dynamic>).should_be(blogsFromModel);
+        }
+
+        void when_navigating_to_create_a_blog()
+        {
+            act = () => actionResult = homeController.New() as ViewResult;
+
+            it["returns view"] = () =>
+                actionResult.should_cast_to<ViewResult>();
+        }
+
+        void when_creating_new_blog()
+        {
+            act = () => actionResult = homeController.New(titleToSave, bodyToSave);
+
+            context["blog is valid"] = () =>
+            {
+                before = () =>
+                {
+                    titleToSave = "some blog title";
+                    bodyToSave = "some body";
+                };
+
+                it["redirects to index after insert"] = () =>
+                {
+                    var redirectResult = actionResult.should_cast_to<RedirectToRouteResult>();
+                    redirectResult.RouteValues["action"].should_be("Index");
+                };
+
+                it["inserts blog"] = () =>
+                {
+                    insertedBlogs.Any(s =>
+                            s.Title == "some blog title" &&
+                            s.Id != Guid.Empty &&
+                            s.Body == "some body")
+                        .should_be_true();
+                };
+            };
+
+            context["invalid blog"] = () =>
+            {
+                before = () => titleToSave = "";
+
+                act = () => viewBag = (actionResult as ViewResult).ViewBag;
+
+                it["returns view, giving user the opportunity to fix error"] = () =>
+                    actionResult.should_cast_to<ViewResult>();
+
+                it["notifies user that title is required"] = () =>
+                    (viewBag.Flash as string).should_be("Title Required.");
+            };
+        }
+    }
+}
---
+++ DynamicBlog/DynamicBlog.Tests/Models/describe_Blog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSpec;
+using DynamicBlog.Models;
+
+namespace DynamicBlog.Tests.Models
+{
+    class describe_Blog : nspec
+    {
+        dynamic blog;
+        string summary;
+
+        void describe_summary()
+        {
+            act = () => summary = blog.Summary;
+
+            context["body is null"] = () =>
+            {
+                before = () => GivenBlog(withBody: null);
+
+                it["summary returns empty string"] = () => summary.should_be("");
+            };
+
+            context["body is under 50 characters"] = () =>
+            {
+                before = () => GivenBlog(withBody: "Blog under 50 characters");
+
+                it["returns body verbatim"] = () => summary.should_be("Blog under 50 characters");
+            };
+
+            context["body over 50 characters"] = () =>
+            {
+                before = () => GivenBlog(withBody: "01234567890123456789012345678901234567890123456789OVER50");
+
+                it["returns the first 50 characters"] = () => summary.should_be("01234567890123456789012345678901234567890123456789");
+            };
+        }
+
+        void GivenBlog(string withBody)
+        {
+            blog = new Blog(new { Body = withBody });
+        }
+    }
+}
