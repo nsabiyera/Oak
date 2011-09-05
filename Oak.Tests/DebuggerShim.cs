d40e78abe38cbd94753b3f5b45fb46767253b895
Amir Rajan <ar@amirrajan.net>
2011-09-05 16:56:16 -0500

    added case insensitivity to all other validators, centralized retrieval of property value

--- Oak.Tests/DebuggerShim.cs
+++ Oak.Tests/DebuggerShim.cs
@@ -13,7 +13,7 @@ namespace DynamicBlog.Tests
         [Test]
         public void debug()
         {
-            var testClassYouWantToDebug = "errors";
+            var testClassYouWantToDebug = "confirmation";
 
             var finder = new SpecFinder(
                 Assembly.GetExecutingAssembly().Location,
