8ef20bd4477fee14d4c7941f1eb46f4767549d75
Amir Rajan <ar@amirrajan.net>
2011-09-05 06:03:26 -0500

    working through confirmation validation

---
+++ Oak.Tests/describe_DynamicModel/SampleClasses/Registration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oak.Models;
+
+namespace Oak.Tests.describe_DynamicModel.SampleClasses
+{
+    public class Registration : DynamicModel
+    {
+        public Registration()
+        {
+            Validates("Email", Presense);
+            Validates("Password", Presense);
+            Validates("Password", Confirmation);
+            Validates("PasswordConfirmation", Presense);
+        }
+    }
+}
--- Oak/Models/Mix.cs
+++ Oak/Models/Mix.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Dynamic;
 using Massive;
+using System.Diagnostics;
 
 namespace Oak
 {
