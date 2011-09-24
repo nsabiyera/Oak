b519f16fa6af969676bfefdbf81787526d5565be
Amir Rajan <ar@amirrajan.net>
2011-09-24 12:08:11 -0500

    moved attribute definition to oakform

--- Oak.Tests/DebuggerShim.cs
+++ Oak.Tests/DebuggerShim.cs
@@ -13,7 +13,7 @@ namespace DynamicBlog.Tests
         [Test]
         public void debug()
         {
-            var testClassYouWantToDebug = "confirmation";
+            var testClassYouWantToDebug = "describe_DynamicForm";
 
             var finder = new SpecFinder(
                 Assembly.GetExecutingAssembly().Location,
--- Oak/DynamicForm.cs
+++ Oak/DynamicForm.cs
@@ -12,7 +12,7 @@ namespace Oak
 {
     public class Hash : Dictionary<string, string>
     {
-        
+
     }
 
     public class DynamicForm : DynamicObject
@@ -41,38 +41,26 @@ namespace Oak
 
             var elements = args[0] as IDictionary<string, string>;
 
-            List<string> elementAttributes = ElementAttributes();
-
-            foreach (var kvp in elements)
-            {
-                if (kvp.Key == "id") metaData.Id = kvp.Value;
-
-                else if (kvp.Key == "value" && metaData.Value == null) metaData.Value = kvp.Value;
-
-                else if (IsAttribute(elementAttributes, kvp)) metaData.Attributes.Add(kvp.Key, kvp.Value.ToString());
-
-                else metaData.Styles.Add(kvp.Key, kvp.Value.ToString());
-            }
+            foreach (var kvp in elements) metaData.Set(kvp.Key, kvp.Value);
 
             return true;
         }
 
-        private static bool IsAttribute(List<string> elementAttributes, KeyValuePair<string, string> kvp)
-        {
-            return elementAttributes.Contains(kvp.Key) || kvp.Key.StartsWith("data-");
-        }
-
         private ElementMetaData NewElementMetatDataFor(string name)
         {
-            return new ElementMetaData
-            {
-                Id = name,
-                Value = PropertyValue(name)
-            };
+            var element = new ElementMetaData();
+
+            element.Set("id", name);
+
+            var value = PropertyValue(name);
+
+            element.Set("value", value == null ? null : value.ToString());
+
+            return element;
         }
 
         private object PropertyValue(string name)
-        {   
+        {
             var property = GetProperty(name);
 
             if (property != null) return property.GetValue(entity as object, null);
@@ -94,50 +82,6 @@ namespace Oak
         private PropertyInfo GetProperty(string name)
         {
             return (entity as object).GetType().GetProperty(name);
-        }
-
-        public List<string> ElementAttributes()
-        {
-            var list = new List<string>();
-
-            list.Add("accept");
-            list.Add("align");
-            list.Add("alt");
-            list.Add("checked");
-            list.Add("disabled");
-            list.Add("maxlength");
-            list.Add("name");
-            list.Add("readonly");
-            list.Add("size");
-            list.Add("src");
-            list.Add("type");
-            list.Add("placeholder");
-
-            list.Add("accesskey");
-            list.Add("class");
-            list.Add("dir");
-            list.Add("id");
-            list.Add("lang");
-            list.Add("style");
-            list.Add("tabindex");
-            list.Add("title");
-
-            list.Add("onblur");
-            list.Add("onchange");
-            list.Add("onclick");
-            list.Add("ondbclick");
-            list.Add("onfocus");
-            list.Add("onmousedown");
-            list.Add("onmousemove");
-            list.Add("onmouseout");
-            list.Add("onmouseover");
-            list.Add("onmouseup");
-            list.Add("onkeydown");
-            list.Add("onkeypress");
-            list.Add("onkeyup");
-            list.Add("onselect");
-
-            return list;
         }
     }
 }
