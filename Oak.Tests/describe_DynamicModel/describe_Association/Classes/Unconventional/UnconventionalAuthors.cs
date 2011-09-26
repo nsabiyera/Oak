3add70b59f13a875962d79dfaca810a32cb945e9
Justin Williams <justinjohnwilliams@gmail.com>
2011-09-26 18:56:35 -0500

    groundwork for tests on has_one

---
+++ Oak.Tests/describe_DynamicModel/describe_Association/Classes/Unconventional/UnconventionalAuthors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Massive;
+
+namespace Oak.Tests.describe_DynamicModel.describe_Association.Classes
+{
+    class UnconventionalAuthors : DynamicRepository
+    {
+        public UnconventionalAuthors()
+            : base("Authors")
+        {
+            Projection = d => new UnconventionalAuthor(d);
+        }
+    }
+}
