9b07b6f71d27ff4e790c65235df2f8085ae14618
ot-chris-holt <cholt@technologies.net>
2011-10-09 14:02:34 -0500

    initial adds for length validation and its tests domain object

---
+++ Oak.Tests/describe_DynamicModel/describe_Validation/length.cs
@@ -0,0 +1,30 @@
+﻿using NSpec;
+using Oak.Tests.describe_DynamicModel.describe_Validation.Classes;
+
+namespace Oak.Tests.describe_DynamicModel.describe_Length
+{
+    class length : nspec
+    {
+        dynamic essay;
+
+        bool isValid;
+
+        void before_each()
+        {
+            essay = new Essay();
+            essay.Title = string.Empty;
+        }
+
+        void validating_length_of()
+        {
+            act = () => isValid = essay.IsValid();
+
+            context["title is not long enough"] = () =>
+            {
+                before = () => essay.Title = string.Empty;
+                
+                it["is not valid"] = () => isValid.should_be_true();
+            };
+        }
+    }
+}
