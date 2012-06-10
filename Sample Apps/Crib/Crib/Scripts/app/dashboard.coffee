0117b2f943c2280c3ea0ed0782c555b66e3b0757
Amir Rajan <ar@amirrajan.net>
2012-06-10 11:53:50 -0500

    reference impl crib: integrating rolloff view into backbone

--- Sample Apps/Crib/Crib/Scripts/app/dashboard.coffee
+++ Sample Apps/Crib/Crib/Scripts/app/dashboard.coffee
@@ -3,9 +3,79 @@ this.dashboard =
     @view = new DashboardView()
 
 DashboardView = Backbone.View.extend
+  el: "#roll_offs"
+
   initialize: ->
+    @rollOffs = new RollOffs()
+
+    @rollOffs.bind 'reset', @render, @
+
+    @refresh()
 
   refresh: ->
+    @rollOffs.fetch()
   
   render: ->
+    $(@el).empty()
 
+    monthSeperator = -1
+
+    consultantContainer = null
+
+    sorted = @rollOffs
+
+    sorted.each (consultant) =>
+      view = new ConsultantView
+        model: consultant
+
+      currentMonth = consultant.rollOffMonth()
+
+      if(monthSeperator != currentMonth)
+        monthSeperator = currentMonth
+        consultantContainer = $("<ul></ul>").addClass("thumbnails").css({ 'margin-top': '10px' })
+        $(@el).append consultantContainer
+
+      consultantContainer.append $("<li></li>").append(view.render().el)
+
+  createSeperator: (month, year) ->
+    
+
+ConsultantView = Backbone.View.extend
+  render: ->
+    $(@el).append $.tmpl(@engageConsultant, { name: @model.name() })
+    return @
+
+  engageConsultant:
+    '
+    <a href="#" class="thumbnail">
+      <img src="http://placehold.it/130x90" alt="">
+    </a>
+    <div class="well" style="margin: 3px; padding: 3px; width: 130px">
+        <div class="btn-group">
+          <a class="btn dropdown-toggle" style="width: 110px" data-toggle="dropdown" href="#">
+            Options
+            <span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a data-toggle="modal" href="#myModal">consultant has been extended</li>
+            <li><a data-toggle="modal" href="#editConsultantModal">edit</a></li>
+          </ul>
+        </div>
+        <h4 style="margin: 5px">${name}</h4>
+    </div>
+    '
+
+RollOff = Backbone.Model.extend
+  monthName: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  name: -> @get("Name")
+
+  rollOffDate: -> new Date(@get("RollOffDate"))
+
+  rollOffMonth: -> @rollOffDate().getMonth()
+
+  rollOffMonthName: -> @monthName[@rollOffMonth()]
+
+RollOffs = Backbone.Collection.extend
+  model: RollOff
+  url: "rolloffs/list"
--- Sample Apps/Crib/Crib/Scripts/app/dashboard.js
+++ Sample Apps/Crib/Crib/Scripts/app/dashboard.js
@@ -1,5 +1,5 @@
 (function() {
-  var DashboardView;
+  var ConsultantView, DashboardView, RollOff, RollOffs;
 
   this.dashboard = {
     init: function() {
@@ -8,9 +8,87 @@
   };
 
   DashboardView = Backbone.View.extend({
-    initialize: function() {},
-    refresh: function() {},
-    render: function() {}
+    el: "#roll_offs",
+    initialize: function() {
+      this.rollOffs = new RollOffs();
+      this.rollOffs.bind('reset', this.render, this);
+      return this.refresh();
+    },
+    refresh: function() {
+      return this.rollOffs.fetch();
+    },
+    render: function() {
+      var consultantContainer, monthSeperator, sorted,
+        _this = this;
+      $(this.el).empty();
+      monthSeperator = -1;
+      consultantContainer = null;
+      sorted = this.rollOffs;
+      return sorted.each(function(consultant) {
+        var currentMonth, view;
+        view = new ConsultantView({
+          model: consultant
+        });
+        currentMonth = consultant.rollOffMonth();
+        if (monthSeperator !== currentMonth) {
+          monthSeperator = currentMonth;
+          consultantContainer = $("<ul></ul>").addClass("thumbnails").css({
+            'margin-top': '10px'
+          });
+          $(_this.el).append(consultantContainer);
+        }
+        return consultantContainer.append($("<li></li>").append(view.render().el));
+      });
+    },
+    createSeperator: function(month, year) {}
+  });
+
+  ConsultantView = Backbone.View.extend({
+    render: function() {
+      $(this.el).append($.tmpl(this.engageConsultant, {
+        name: this.model.name()
+      }));
+      return this;
+    },
+    engageConsultant: '\
+    <a href="#" class="thumbnail">\
+      <img src="http://placehold.it/130x90" alt="">\
+    </a>\
+    <div class="well" style="margin: 3px; padding: 3px; width: 130px">\
+        <div class="btn-group">\
+          <a class="btn dropdown-toggle" style="width: 110px" data-toggle="dropdown" href="#">\
+            Options\
+            <span class="caret"></span>\
+          </a>\
+          <ul class="dropdown-menu">\
+            <li><a data-toggle="modal" href="#myModal">consultant has been extended</li>\
+            <li><a data-toggle="modal" href="#editConsultantModal">edit</a></li>\
+          </ul>\
+        </div>\
+        <h4 style="margin: 5px">${name}</h4>\
+    </div>\
+    '
+  });
+
+  RollOff = Backbone.Model.extend({
+    monthName: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    name: function() {
+      return this.get("Name");
+    },
+    rollOffDate: function() {
+      return new Date(this.get("RollOffDate"));
+    },
+    rollOffMonth: function() {
+      return this.rollOffDate().getMonth();
+    },
+    rollOffMonthName: function() {
+      return this.monthName[this.rollOffMonth()];
+    }
+  });
+
+  RollOffs = Backbone.Collection.extend({
+    model: RollOff,
+    url: "rolloffs/list"
   });
 
 }).call(this);
