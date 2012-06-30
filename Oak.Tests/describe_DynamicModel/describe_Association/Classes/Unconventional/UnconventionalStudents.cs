b2561f568c9510ea06e41937de3194d51c62a6c4
Amir Rajan <ar@amirrajan.net>
2012-06-30 13:15:55 -0500

    updated hasmanyandbelongs to support unconventional foreign keys and table names

---
+++ Oak.Tests/describe_DynamicModel/describe_Association/Classes/Unconventional/UnconventionalStudents.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Massive;
+
+namespace Oak.Tests.describe_DynamicModel.describe_Association.Classes.Unconventional
+{
+    class UnconventionalStudents : DynamicRepository
+    {
+        public UnconventionalStudents()
+            : base("Students")
+        {
+            Projection = d => new UnconventionalStudent(d);
+        }
+    }
+
+    class UnconventionalCourses : DynamicRepository
+    {
+        public UnconventionalCourses()
+            : base("Courses")
+        {
+            
+        }
+    }
+
+    class UnconventionalStudent : DynamicModel
+    {
+        UnconventionalStudents students = new UnconventionalStudents();
+
+        UnconventionalCourses courses = new UnconventionalCourses();
+
+        public UnconventionalStudent(object dto)
+            : base(dto)
+        {
+            
+        }
+
+        IEnumerable<dynamic> Associates()
+        {
+            yield return new HasManyAndBelongsTo(courses, students)
+            {
+                Named = "Courses",
+                CrossRefenceTable = "StudentsCourses",
+                FromColumn = "StudentId",
+                ForeignKey = "CourseId"
+            };
+        }
+    }
+}
--- Oak.Tests/describe_DynamicModel/describe_Association/has_many_and_belongs_to.cs
+++ Oak.Tests/describe_DynamicModel/describe_Association/has_many_and_belongs_to.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using NSpec;
 using Oak.Tests.describe_DynamicModel.describe_Association.Classes;
+using Oak.Tests.describe_DynamicModel.describe_Association.Classes.Unconventional;
 
 namespace Oak.Tests.describe_DynamicModel.describe_Association
 {
-    [Tag("wip")]
     class has_many_and_belongs_to : nspec
     {
         Seed seed;
@@ -31,17 +31,7 @@ namespace Oak.Tests.describe_DynamicModel.describe_Association
             {
                 seed.PurgeDb();
 
-                seed.CreateTable("Students", new dynamic[] 
-                { 
-                    seed.Id(),
-                    new { Name = "nvarchar(255)" }
-                }).ExecuteNonQuery();
-
-                seed.CreateTable("Courses", new dynamic[] 
-                { 
-                    seed.Id(),
-                    new { Name = "nvarchar(255)" }
-                }).ExecuteNonQuery();
+                BuildCommonTables();
 
                 seed.CreateTable("CoursesStudents", new dynamic[] 
                 { 
@@ -57,7 +47,7 @@ namespace Oak.Tests.describe_DynamicModel.describe_Association
                 new { studentId, courseId }.InsertInto("CoursesStudents");
             };
 
-            it["cross reference table is determined by convention (tables are alphabetically combined)"] = () =>
+            it["cross reference table is determined by convention (tables are alphabetically combined)", "wip"] = () =>
                 (FirstCourseFor(studentId).Name as string).should_be("History");
 
             it["has a reference back to the original table"] = () =>
@@ -96,6 +86,69 @@ namespace Oak.Tests.describe_DynamicModel.describe_Association
                     newCount.should_be(2);
                 };
             };
+
+            context["retrieving ids"] = () =>
+            {
+                it["contains just ids"] = () =>
+                {
+                    student = students.Single(studentId);
+
+                    var ids = student.CourseIds() as IEnumerable<dynamic>;
+
+                    (ids.First() as object).should_be(courseId as object);
+                };
+            };
+        }
+
+        [Tag("wip")]
+        void describe_overrides()
+        {
+            context["xref table can be overridden"] = () =>
+            {
+                before = () =>
+                {
+                    seed.PurgeDb();
+
+                    BuildCommonTables();
+
+                    seed.CreateTable("StudentsCourses", new dynamic[] 
+                    { 
+                        seed.Id(),
+                        new { CourseId = "int" },
+                        new { StudentId = "int" }
+                    }).ExecuteNonQuery();
+
+                    studentId = new { Name = "Amir" }.InsertInto("Students");
+
+                    courseId = new { Name = "History" }.InsertInto("Courses");
+
+                    new { studentId, courseId }.InsertInto("StudentsCourses");
+                };
+
+                it["works"] = () =>
+                {
+                    UnconventionalStudents uStudents = new UnconventionalStudents();
+
+                    var student = uStudents.Single(studentId);
+
+                    (student.Courses().First().Name as string).should_be("History");
+                };
+            };
+        }
+
+        void BuildCommonTables()
+        {
+            seed.CreateTable("Students", new dynamic[] 
+                { 
+                    seed.Id(),
+                    new { Name = "nvarchar(255)" }
+                }).ExecuteNonQuery();
+
+            seed.CreateTable("Courses", new dynamic[] 
+                { 
+                    seed.Id(),
+                    new { Name = "nvarchar(255)" }
+                }).ExecuteNonQuery();
         }
 
         dynamic CoursesFor(object studentId)
