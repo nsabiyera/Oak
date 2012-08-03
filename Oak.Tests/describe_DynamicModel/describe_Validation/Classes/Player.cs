50b60c5eb02a52377e50046554dd5f147d47a438
Amir Rajan <ar@amirrajan.net>
2012-08-02 21:31:56 -0500

    more tests around using auto props for validation

--- Oak.Tests/describe_DynamicModel/describe_Validation/Classes/Player.cs
+++ Oak.Tests/describe_DynamicModel/describe_Validation/Classes/Player.cs
@@ -2,6 +2,38 @@
 
 namespace Oak.Tests.describe_DynamicModel.describe_Validation.Classes
 {
+    public class PlayerWithAutoProps : DynamicModel
+    {
+        public PlayerWithAutoProps()
+            : this(new { })
+        {
+        }
+
+        public PlayerWithAutoProps(object dto)
+            : base(dto)
+        {
+        }
+
+        public double AveragePoints { get; set; }
+        public double Age { get; set; }
+        public double HeightInInches { get; set; }
+        public double WeightInPounds { get; set; }
+        public double NumberOfFingers { get; set; }
+        public double LuckyEvenNumber { get; set; }
+        public double LuckyOddNumber { get; set; }
+
+        public IEnumerable<dynamic> Validates()
+        {
+            yield return new Numericality("AveragePoints");
+            yield return new Numericality("Age") { OnlyInteger = true };
+            yield return new Numericality("HeightInInches") { GreaterThan = 60, LessThan = 72 };
+            yield return new Numericality("WeightInPounds") { GreaterThanOrEqualTo = 185, LessThanOrEqualTo = 300 };
+            yield return new Numericality("NumberOfFingers") { EqualTo = 10 };
+            yield return new Numericality("LuckyEvenNumber") { Even = true };
+            yield return new Numericality("LuckyOddNumber") { Odd = true };
+        }
+    }
+
     public class Player : DynamicModel
     {
         public Player()
--- Oak.Tests/describe_DynamicModel/describe_Validation/Classes/User.cs
+++ Oak.Tests/describe_DynamicModel/describe_Validation/Classes/User.cs
@@ -2,6 +2,29 @@ using System.Collections.Generic;
 
 namespace Oak.Tests.describe_DynamicModel.describe_Validation.Classes
 {
+    public class UserWithAutoProperties : DynamicModel
+    {
+        Users users;
+
+        public UserWithAutoProperties()
+            : this(new { })
+        {
+        }
+
+        public UserWithAutoProperties(object dto)
+            : base(dto)
+        {
+            users = new Users();
+        }
+
+        public IEnumerable<dynamic> Validates()
+        {
+            yield return new Uniqueness("Email", users);
+        }
+
+        public string Email { get; set; }
+    }
+    
     public class User : DynamicModel
     {
         Users users;
