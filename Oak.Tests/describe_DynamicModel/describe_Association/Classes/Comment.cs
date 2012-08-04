0889e3fc8bef9771b526594d5900d9c89330b3ec
Amir Rajan <ar@amirrajan.net>
2012-08-03 20:43:59 -0500

    tests to ensure static types work with belongs to after initialization

--- Oak.Tests/describe_DynamicModel/describe_Association/Classes/Comment.cs
+++ Oak.Tests/describe_DynamicModel/describe_Association/Classes/Comment.cs
@@ -5,6 +5,31 @@ using System.Text;
 
 namespace Oak.Tests.describe_DynamicModel.describe_Association.Classes
 {
+    public class CommentWithAutoProps : DynamicModel
+    {
+        Blogs blogs = new Blogs();
+
+        public CommentWithAutoProps(object dto)
+            : base(dto)
+        {
+            
+        }
+        public CommentWithAutoProps()
+        {
+            
+        }
+
+        public IEnumerable<dynamic> Associates()
+        {
+            yield return
+            new BelongsTo(blogs);
+        }
+
+        public int Id { get; set; }
+        public int BlogId { get; set; }
+        public string Text { get; set; }
+    }
+
     public class Comment : DynamicModel
     {
         Blogs blogs = new Blogs();
