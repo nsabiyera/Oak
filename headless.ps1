bb2e23e17e6835f2e856a7f57887c8256a3c1771
Amir Rajan <ar@amirrajan.net>
2012-08-26 11:35:49 -0500

    updated oak-edge, working through headless mode for sample apps

---
+++ headless.ps1
@@ -0,0 +1,13 @@
+Start-Service winrm
+$nlm = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
+$connections = $nlm.getnetworkconnections()
+$connections |foreach {
+    if ($_.getnetwork().getcategory() -eq 0)
+    {
+        $_.getnetwork().setcategory(1)
+    }
+}
+Enable-PSRemoting -force
+set-item wsman:localhost\Shell\MaxMemoryPerShellMB 2048
+icm -computer localhost { C:\development\oak\sample apps\borrowedgames\borrowedgames.ui.tests\bin\debug\borrowedgames.ui.tests.exe }
+
