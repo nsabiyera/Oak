d3181ca8e45846786bbb095e70a4f1984f3980d0
Amir Rajan <ar@amirrajan.net>
2012-08-26 11:35:49 -0500

    updated oak-edge, working through headless mode for sample apps

--- Sample Apps/BorrowedGames/BorrowedGames/Scripts/app/requested.js
+++ Sample Apps/BorrowedGames/BorrowedGames/Scripts/app/requested.js
@@ -26,9 +26,7 @@
     shortName: function() {
       var name;
       name = this.name();
-      if (name.length > 41) {
-        name = name.substring(0, 40) + "... ";
-      }
+      if (name.length > 41) name = name.substring(0, 40) + "... ";
       return name += " (" + this.console() + ")";
     },
     giveGame: function(callback) {
@@ -111,12 +109,8 @@
     },
     render: function() {
       var game;
-      if (this.model.canGiveGame()) {
-        game = this.genCanGiveTemplate();
-      }
-      if (!this.model.canGiveGame()) {
-        game = this.genReturnGame();
-      }
+      if (this.model.canGiveGame()) game = this.genCanGiveTemplate();
+      if (!this.model.canGiveGame()) game = this.genReturnGame();
       $(this.el).html(game);
       return this;
     },
