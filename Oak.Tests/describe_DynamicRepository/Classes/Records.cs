8b92da86309fb46c9cc86e3353a61b6a224ae15a
Amir Rajan <ar@amirrajan.net>
2012-12-08 13:28:20 -0600

    added test to demonstrate memoization for dynamic repositories

--- Oak.Tests/describe_DynamicRepository/Classes/Records.cs
+++ Oak.Tests/describe_DynamicRepository/Classes/Records.cs
@@ -7,4 +7,27 @@ using Massive;
 namespace Oak.Tests.describe_DynamicRepository.Classes
 {
     public class Records : DynamicRepository { }
+
+    public class MemoizedRecords : DynamicRepository
+    {
+        public MemoizedRecords() : base("Records")
+        {
+            
+        }
+
+        static MemoizedRecords()
+        {
+            Gemini.Extend<MemoizedRecords, Memoize>();
+        }
+
+        IEnumerable<dynamic> Memoize()
+        {
+            yield return (DynamicFunctionWithParam)CurrentRecords;
+        }
+
+        dynamic CurrentRecords(dynamic startingWith)
+        {
+            return All(where: "Name like @0", args: new object[] { startingWith + "%" });
+        }
+    }
 }
