06e6cccd1478ad3f9cefcdae2132785eeab3b3f2
Amir Rajan <ar@amirrajan.net>
2013-04-25 11:27:33 -0500

    peeps implementation of emberjs, half backed unfortunately

--- Sample Apps/Peeps/Peeps/Scripts/ember-app/app.js
+++ Sample Apps/Peeps/Peeps/Scripts/ember-app/app.js
@@ -1,19 +1,10 @@
 ﻿window.App = Ember.Application.create({ rootElement: "#dashboard" });
 
-App.Store = DS.Store.extend({
-  revision: 12,
-  adapter: 'DS.MvcRestAdapter'
-});
+App.Store = DS.Store.extend({ revision: 12, adapter: 'DS.MvcRestAdapter' });
 
-
-App.Router.map(function() {
-  this.resource('peeps', { path: '/' });
-});
+App.Router.map(function() { this.resource('peeps', { path: '/' }); });
 
 App.PeepsRoute = Ember.Route.extend({
-  setupController: function(controller, model) {
-    controller.set('content', model);
-  },
   model: function() {
     return App.Peep.find();
   }
@@ -22,7 +13,6 @@ App.PeepsRoute = Ember.Route.extend({
 App.Peep = DS.Model.extend({
   name: DS.attr('string')
 });
-
 
 App.PeepsController = Ember.ArrayController.extend({
   saveAll: function() {
@@ -34,3 +24,4 @@ App.PeepsController = Ember.ArrayController.extend({
 		});
   }
 });
+
