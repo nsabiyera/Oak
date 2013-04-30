a60d09643d6958d47e33bc61c8286fafc20bfc11
Amir Rajan <ar@amirrajan.net>
2013-04-30 15:29:53 -0500

    minor refactoring to ko template, still working through ember implementation

--- Sample Apps/Peeps/Peeps/Scripts/ember-app/app.js
+++ Sample Apps/Peeps/Peeps/Scripts/ember-app/app.js
@@ -2,6 +2,68 @@
 
 App.Router.map(function() { this.resource('peeps', { path: '/' }); });
 
+App.RESTAdapter = Ember.Adapter.extend({
+  saveRecord: function(record) {
+    var data, klass, rootKey,
+      _this = this;
+    klass = record.constructor;
+    rootKey = Ember.get(klass, 'rootKey');
+    data = {};
+    data[rootKey] = record.toJSON();
+    $.ajax("" + (Ember.get(klass, 'url')) + "/" + (record.get('id')) + ".json", {
+      type: 'PUT',
+      data: data
+    }).then(function(data) {
+      Ember.run(function() {
+        return record.set('isSaving', false);
+      });
+    });
+  },
+  createRecord: function(record) {
+    var data, klass, rootKey,
+      _this = this;
+    klass = record.constructor;
+    data = record.toJSON();
+    $.ajax(Ember.get(klass, 'post_url'), {
+      type: 'POST',
+      data: data
+    }).then(function(data) {
+      Ember.run(function() {
+        return record.load(data);
+      });
+    });
+  },
+  deleteRecord: function(record) {
+    var klass, url,
+      _this = this;
+    klass = record.constructor;
+    url = "" + (Ember.get(klass, 'url')) + "/" + (record.get('id')) + ".json";
+    $.ajax(url, {
+      type: 'DELETE'
+    }).then(function(data) {
+      Ember.run(function() {
+        return record.didDeleteRecord();
+      });
+    });
+  },
+  findAll: function(klass, recordArray) {
+    var _this = this;
+    $.getJSON((Ember.get(klass, 'get_url')), {}, function(data) {
+      Ember.run(function() {
+        return recordArray.load(klass, data);
+      });
+    });
+  },
+  find: function(record, id) {
+    $.getJSON("" + (Ember.get(record.constructor, 'url')) + "/" + id + ".json", {}, function(data) {
+      var _this = this;
+      Ember.run(function() {
+        return record.load(data);
+      });
+    });
+  }
+});
+
 App.PeepsRoute = Ember.Route.extend({
   model: function() {
     return App.Peep.find();
@@ -11,42 +73,27 @@ App.PeepsRoute = Ember.Route.extend({
 var attr = Ember.attr;
 
 App.Peep = Ember.Model.extend({
-  name: attr()
+  name: attr(),
 });
 
-App.Peep.adapter = Ember.Adapter.create({
-  findAll: function(klass, recordArray) {
-    $.getJSON("/home/list").then(function(data) {
-      recordArray.load(klass, data);
-    });
-  },
-  createRecord: function(record) {
-    var klass = record.constructor();
-    var rootKey = Ember.get(klass, '');
-    var data = record.toJSON();
-    $.ajax("/home/update", {
-      type: 'POST',
-      data: data
-    }).then(function(data) {
-      Ember.run(function() { record.load(data); });
-    });
-  }
+App.Peep.reopenClass({
+  get_url: "/home/list",
+  post_url: "/home/update"
 });
+
+App.Peep.adapter = new App.RESTAdapter();
 
 App.PeepsController = Ember.ArrayController.extend({
   saveAll: function() {
-    this.get('store').commit();
+
   },
   add: function() {
     debugger;
-    var peep = App.Peep.createRecord({
+
+    var peep = App.Peep.create({
       name: ""
     });
-    // this throws an exception 'has no method createRecord'
-    /*
-       var peep = App.Peep.createRecord({
-       name: ""
-       });
-       */
+
+    App.Peep.adapter.createRecord(peep);
   }
 });
