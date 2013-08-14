3696591af5ee67b6fdddda327ee15417fa346e14
Amir Rajan <ar@amirrajan.net>
2013-08-14 17:29:07 -0500

    cursory sample working for ember.js...not perfect

--- Sample Apps/Peeps/Peeps/Scripts/ember-app/app.js
+++ Sample Apps/Peeps/Peeps/Scripts/ember-app/app.js
@@ -2,11 +2,53 @@
 
 App.Router.map(function() { this.resource('peeps', { path: '/' }); });
 
+App.PeepsRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    controller.getAll();
+  }
+});
+
+App.Peep = Ember.Object.extend({
+  attributes: ["id","name"],
+});
+
+App.PeepController = Ember.ObjectController.extend({
+  save: function() {
+    var peep = this.get('model');
+    $.post("/home/update", peep.getProperties(peep.attributes), function(data) {
+      peep.set("id", data.id);
+    });
+  }
+});
+
 App.PeepsController = Ember.ArrayController.extend({
   saveAll: function() {
-    debugger;
+    var _this = this;
+    var peeps = _.map(this.get('content'), function(peep) {
+      return peep.getProperties(peep.attributes);
+    });
+
+    $.ajax({
+        type: 'POST',
+        url: "/home/updateall",
+        data: JSON.stringify(peeps),
+        success: function() {
+          _this.getAll();
+        },
+        contentType: 'application/json'
+    });
+  },
+  getAll: function() {
+    var controller = this;
+    $.getJSON('/home/list', function (data) {
+      controller.clear();
+      var peeps = _.map(data, function(entry) {
+        return App.Peep.create(entry);
+      });
+      controller.pushObjects(peeps);
+    });
   },
   add: function() {
-    debugger;
-  }
+    this.pushObject(App.Peep.create({ name: "worky" }));
+  },
 });
