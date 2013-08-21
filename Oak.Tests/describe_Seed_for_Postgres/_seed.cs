276fcc11d7cc6a15dca758d72952c5de0f1645a1
Amir Rajan <ar@amirrajan.net>
2013-08-21 12:26:49 -0500

    purgedb for postgre verified for primary keys

---
+++ Oak.Tests/describe_Seed_for_Postgres/_seed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using NSpec;
+using System.Data;
+using Npgsql;
+
+namespace Oak.Tests.describe_Seed_for_Postgres
+{
+    class _seed : nspec
+    {
+        protected Seed seed;
+        protected string command;
+        protected dynamic[] columns;
+        ConnectionProfile connectionProfile;
+        string connectionString = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=Postgres1234;Database=Oak;";
+
+        void before_each()
+        {
+            connectionProfile = new ConnectionProfile
+            {
+                ConnectionString = connectionString,
+                ProviderName = "Npgsql"
+            };
+
+            seed = new Seed(connectionProfile);
+        }
+
+
+        public void CommandShouldBe(string expected)
+        {
+            command.should_be(ToSingleLine(expected));
+        }
+
+        public string ToSingleLine(string s)
+        {
+            var single = Regex.Replace(s, @"[ ]{2,}", "");
+
+            single = single.Trim().Replace(Environment.NewLine, "");
+
+            return single;
+        }
+    }
+}
---
+++ Oak.Tests/describe_Seed_for_Postgres/when_creating_table.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSpec;
+using System.Data;
+using Npgsql;
+
+namespace Oak.Tests.describe_Seed_for_Postgres
+{
+    class when_creating_table : _seed
+    {
+        void act_each()
+        {
+            command = seed.CreateTable("Users", columns);
+        }
+
+        void generating_a_table_where_column_is_an_identity_column_and_primary_key()
+        {
+            before = () =>
+                columns = new dynamic[]
+                {
+                    new { Id = "SERIAL", Identity = true, PrimaryKey = true },
+                };
+
+            it["contains identity definition"] = () =>
+                CommandShouldBe(@"
+                    CREATE TABLE public.Users
+                    (                                                 
+                      Id SERIAL NOT NULL, 
+                      CONSTRAINT PK_Users PRIMARY KEY 
+                      (
+                        Id
+                      )                                             
+                    )
+                ");
+        }
+    }
+}
