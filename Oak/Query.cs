a1f22d038bd1e08a6c2c7c1f4759e23303fdf257
David Podhola <david.podhola@naseukoly.cz>
2013-09-03 10:47:29 +0200

    added using to Query, no locking when program fails

--- Oak/Query.cs
+++ Oak/Query.cs
@@ -13,37 +13,49 @@ namespace Oak
         {
             if (connectionProfile == null) connectionProfile = new ConnectionProfile();
 
-            SqlCeCommand sqlCommand = new SqlCeCommand();
-            sqlCommand.Connection = new SqlCeConnection(connectionProfile.ConnectionString);
-            sqlCommand.Connection.Open();
-            sqlCommand.CommandText = String.Format(query);
-            sqlCommand.ExecuteNonQuery();
-            sqlCommand.Connection.Close();
+            using (SqlCeCommand sqlCommand = new SqlCeCommand())
+            {
+                using (sqlCommand.Connection = new SqlCeConnection(connectionProfile.ConnectionString))
+                {
+                    sqlCommand.Connection.Open();
+                    sqlCommand.CommandText = String.Format(query);
+                    sqlCommand.ExecuteNonQuery();
+                    sqlCommand.Connection.Close();
+                }
+            }
         }
 
         public static object ExecuteScalar(this string query, ConnectionProfile connectionProfile = null)
         {
             if (connectionProfile == null) connectionProfile = new ConnectionProfile();
 
-            SqlCeCommand sqlCommand = new SqlCeCommand();
-            sqlCommand.Connection = new SqlCeConnection(connectionProfile.ConnectionString);
-            sqlCommand.Connection.Open();
-            sqlCommand.CommandText = String.Format(query);
-            var result = sqlCommand.ExecuteScalar();
-            sqlCommand.Connection.Close();
+            using (SqlCeCommand sqlCommand = new SqlCeCommand())
+            {
+                using (sqlCommand.Connection = new SqlCeConnection(connectionProfile.ConnectionString))
+                {
+                    sqlCommand.Connection.Open();
+                    sqlCommand.CommandText = String.Format(query);
+                    var result = sqlCommand.ExecuteScalar();
+                    sqlCommand.Connection.Close();
 
-            return result;
+                    return result;
+                }
+            }
         }
 
         public static SqlCeDataReader ExecuteReader(this string query, ConnectionProfile connectionProfile = null)
         {
             if (connectionProfile == null) connectionProfile = new ConnectionProfile();
 
-            SqlCeCommand sqlCommand = new SqlCeCommand();
-            sqlCommand.Connection = new SqlCeConnection(connectionProfile.ConnectionString);
-            sqlCommand.Connection.Open();
-            sqlCommand.CommandText = String.Format(query);
-            return sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            using (SqlCeCommand sqlCommand = new SqlCeCommand())
+            {
+                using (sqlCommand.Connection = new SqlCeConnection(connectionProfile.ConnectionString))
+                {
+                    sqlCommand.Connection.Open();
+                    sqlCommand.CommandText = String.Format(query);
+                    return sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                }
+            }
         }
     }
 }
