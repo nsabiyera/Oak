7a8ac7febbc927d722a643d6c68829868e612a85
Amir Rajan <ar@amirrajan.net>
2013-10-28 16:31:28 -0500

    skipped tests for schema generation and sqlce

--- Oak.Tests/describe_Seed/schemas.cs
+++ Oak.Tests/describe_Seed/schemas.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using NSpec;
 using Massive;
+using System.Configuration;
 
 namespace Oak.Tests.describe_Seed
 {
@@ -14,131 +15,129 @@ namespace Oak.Tests.describe_Seed
 
     class schemas : _seed
     {
-        void specify_create_table()
+        void for_mssql()
         {
-            seed.PurgeDb();
+            if(IsSqlCe()) return; //these tests are not applicable for SQLCE
 
-            seed.CreateSchema("Logging").ExecuteNonQuery();
+            before = () =>
+            {
+                seed.PurgeDb();
+                seed.CreateSchema("Logging").ExecuteNonQuery();
+            };
 
-            seed.CreateTable("Logging", "Table1", new dynamic[] 
-            { 
-                seed.Id(),
-                new { Name = "nvarchar(255)" }
-            }).ExecuteNonQuery();
+            it["creates table"] = () =>
+            {
+                seed.CreateTable("Logging", "Table1", new dynamic[] 
+                { 
+                    seed.Id(),
+                    new { Name = "nvarchar(255)" }
+                }).ExecuteNonQuery();
 
-            new { Name = "name 1" }.InsertInto("Logging.[Table1]");
+                new { Name = "name 1" }.InsertInto("Logging.[Table1]");
 
-            var repo = new Table1();
+                var repo = new Table1();
 
-            repo.All().Count().should_be(1);
+                repo.All().Count().should_be(1);
+            };
+
+            it["ads column"] = () =>
+            {
+                seed.CreateTable("Logging", "Table1", new dynamic[] 
+                { 
+                    seed.Id(),
+                    new { Name = "nvarchar(255)" }
+                }).ExecuteNonQuery();
+
+                seed.AddColumns("Logging", 
+                    "Table1", 
+                    new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
+
+                new { Name = "name 1", NickName = "nick name" }.InsertInto("Logging.[Table1]");
+
+                var repo = new Table1();
+
+                var firstRecord = repo.All().First();
+
+                repo.All().Count().should_be(1);
+
+                (firstRecord.Name as string).should_be("name 1");
+
+                (firstRecord.NickName as string).should_be("nick name");
+            };
+
+            it["drops column"] = () =>
+            {
+                seed.CreateTable("Logging", "Table1", new dynamic[] 
+                { 
+                    seed.Id(),
+                    new { Name = "nvarchar(255)" }
+                }).ExecuteNonQuery();
+
+                seed.AddColumns("Logging",
+                    "Table1",
+                    new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
+
+                seed.DropColumn("Logging", "Table1", "Name").ExecuteNonQuery();
+
+                new { NickName = "nick name" }.InsertInto("Logging.[Table1]");
+
+                var repo = new Table1();
+
+                var firstRecord = repo.All().First();
+
+                ((bool)firstRecord.RespondsTo("Name")).should_be(false);
+            };
+
+            it["renames column"] = () =>
+            {
+                seed.CreateTable("Logging", "Table1", new dynamic[] 
+                { 
+                    seed.Id(),
+                    new { Name = "nvarchar(255)" }
+                }).ExecuteNonQuery();
+
+                seed.AddColumns("Logging",
+                    "Table1",
+                    new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
+
+                new { NickName = "nick name" }.InsertInto("Logging.[Table1]");
+
+                seed.RenameColumn("Logging", "Table1", "NickName", "NickName2").ExecuteNonQuery();
+
+                var repo = new Table1();
+
+                var firstRecord = repo.All().First();
+
+                ((bool)firstRecord.RespondsTo("NickName")).should_be(false);
+
+                ((bool)firstRecord.RespondsTo("NickName2")).should_be(true);
+            };
+
+            it["drops constraints"] = () =>
+            {
+                seed.CreateTable("Logging", "Table1", new dynamic[] 
+                { 
+                    seed.Id(),
+                    new { Name = "nvarchar(255)" }
+                }).ExecuteNonQuery();
+
+                seed.AddColumns("Logging",
+                    "Table1",
+                    new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
+
+                new { NickName = "nick name" }.InsertInto("Logging.[Table1]");
+
+                seed.DropConstraint("Logging", "Table1", "Id").ExecuteNonQuery();
+
+                seed.DropColumn("Logging", "Table1", "Id").ExecuteNonQuery();
+            };
         }
 
-        void specify_add_column()
+        void for_sqlce()
         {
-            seed.PurgeDb();
+            if(!IsSqlCe()) return; //these tests are only applicable for SQLCE
 
-            seed.CreateSchema("Logging").ExecuteNonQuery();
-
-            seed.CreateTable("Logging", "Table1", new dynamic[] 
-            { 
-                seed.Id(),
-                new { Name = "nvarchar(255)" }
-            }).ExecuteNonQuery();
-
-            seed.AddColumns("Logging", 
-                "Table1", 
-                new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
-
-            new { Name = "name 1", NickName = "nick name" }.InsertInto("Logging.[Table1]");
-
-            var repo = new Table1();
-
-            var firstRecord = repo.All().First();
-
-            repo.All().Count().should_be(1);
-
-            (firstRecord.Name as string).should_be("name 1");
-
-            (firstRecord.NickName as string).should_be("nick name");
-        }
-
-        void specify_drop_column()
-        {
-            seed.PurgeDb();
-
-            seed.CreateSchema("Logging").ExecuteNonQuery();
-
-            seed.CreateTable("Logging", "Table1", new dynamic[] 
-            { 
-                seed.Id(),
-                new { Name = "nvarchar(255)" }
-            }).ExecuteNonQuery();
-
-            seed.AddColumns("Logging",
-                "Table1",
-                new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
-
-            seed.DropColumn("Logging", "Table1", "Name").ExecuteNonQuery();
-
-            new { NickName = "nick name" }.InsertInto("Logging.[Table1]");
-
-            var repo = new Table1();
-
-            var firstRecord = repo.All().First();
-
-            ((bool)firstRecord.RespondsTo("Name")).should_be(false);
-        }
-
-        void specify_rename_column()
-        {
-            seed.PurgeDb();
-
-            seed.CreateSchema("Logging").ExecuteNonQuery();
-
-            seed.CreateTable("Logging", "Table1", new dynamic[] 
-            { 
-                seed.Id(),
-                new { Name = "nvarchar(255)" }
-            }).ExecuteNonQuery();
-
-            seed.AddColumns("Logging",
-                "Table1",
-                new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
-
-            new { NickName = "nick name" }.InsertInto("Logging.[Table1]");
-
-            seed.RenameColumn("Logging", "Table1", "NickName", "NickName2").ExecuteNonQuery();
-
-            var repo = new Table1();
-
-            var firstRecord = repo.All().First();
-
-            ((bool)firstRecord.RespondsTo("NickName")).should_be(false);
-
-            ((bool)firstRecord.RespondsTo("NickName2")).should_be(true);
-        }
-
-        void specify_drop_constraint()
-        {
-            seed.PurgeDb();
-
-            seed.CreateSchema("Logging").ExecuteNonQuery();
-
-            seed.CreateTable("Logging", "Table1", new dynamic[] 
-            { 
-                seed.Id(),
-                new { Name = "nvarchar(255)" }
-            }).ExecuteNonQuery();
-
-            seed.AddColumns("Logging",
-                "Table1",
-                new { NickName = "nvarchar(255)" }).ExecuteNonQuery();
-
-            new { NickName = "nick name" }.InsertInto("Logging.[Table1]");
-
-            seed.DropConstraint("Logging", "Table1", "Id").ExecuteNonQuery();
-
-            seed.DropColumn("Logging", "Table1", "Id").ExecuteNonQuery();
+            it["gives error that sqlce isn't supported"] = todo;
         }
     }
 }
