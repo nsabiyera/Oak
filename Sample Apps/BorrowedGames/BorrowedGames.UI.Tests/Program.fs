4141bc85917b227ac9783d92b22445196df71ed2
Amir Rajan <ar@amirrajan.net>
2014-03-11 10:56:03 -0500

    updated canopy for borrowed games and task rabbits

--- Sample Apps/BorrowedGames/BorrowedGames.UI.Tests/Program.fs
+++ Sample Apps/BorrowedGames/BorrowedGames.UI.Tests/Program.fs
@@ -3,40 +3,41 @@ open runner
 open uimethods
 open setupmethods
 open configuration
-open reporters
 
 start firefox
-
-let (====) a b = a &&& b
 
 context "borrowedgames.com"
 
 before(fun _ -> logOff())
 
-"borrower returns game" 
-==== fun _ ->
+test(fun _ ->
+    describe "borrower returns game" 
     requestGame()
     logOff()
     loginAs "user2"
     wantedGamesList *= "currently borrowing"
     click deleteBorrowedGameLink
     wantedGamesList *!= "currently borrowing"
+)
 
-"registering a user" 
-==== fun _ -> 
+test(fun _ ->
+    describe "registering a user" 
     reset()
     registerUser "user1"
+)
 
-"request games" 
-==== fun _ -> 
+test(fun _ ->
+    describe "request games" 
     requestGame()
+)
 
-"lender marks game as returned" 
-==== fun _ ->
+test(fun _ ->
+    describe "lender marks game as returned"
     requestGame()
     loginAs "user1"
     click gameReturnedLink
     count "#requestedGames i" 0
+)
 
 run()
 
